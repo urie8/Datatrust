aaf7f9f448f40d16999c21e436c82dfcc1b85871
dionp <dionpeci08@gmail.com>
2025-02-03 11:51:06 +0100

    Initial

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using DataTrust.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DataTrust.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext database;
+
+        public IndexModel(AppDbContext database)
+        {
+            this.database = database;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
